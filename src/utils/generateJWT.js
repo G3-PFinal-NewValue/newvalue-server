153a3ee18ab28c6f97f6340eefee372c4c520d4d
MichelleGel <michellegelves@gmail.com>
2025-10-22 15:33:35 +0200

    Fix es modules on generateJWT

--- src/utils/generateJWT.js
+++ src/utils/generateJWT.js
@@ -1,13 +1,16 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-const generateJWT = (user) => {
-  // Incluye el rol en el JWT si está disponible.  Si no se proporciona rol,
-  // se puede usar un valor por defecto.  TODO: confirmar lógica de roles.
+dotenv.config();
+
+export const generateJWT = (user) => {
+  // Incluye el rol en el JWT si está disponible.
+  // Si no se proporciona rol, se usa un valor por defecto.
   const payload = {
     id: user.id,
     email: user.email,
-    role: user.role || 'User'
+    role: user.role || 'User',
   };
+
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
