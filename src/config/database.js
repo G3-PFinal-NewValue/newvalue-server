56ecf88ef936d037c40edc6b87e1b173509e8b16
luisasilva99 <luisasilvamartinez1999@gmail.com>
2025-10-22 12:04:54 +0000

    changes to up server

--- src/config/database.js
+++ src/config/database.js
@@ -26,3 +26,5 @@ export const connectDB = async () => {
   }
 };
 
+
+
--- src/models/RoleModel.js
+++ src/models/RoleModel.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/database.js";
+import { sequelize } from "../config/database.js";
 import UserModel from "./UserModel.js";
 
 const RoleModel = sequelize.define("Role", {
