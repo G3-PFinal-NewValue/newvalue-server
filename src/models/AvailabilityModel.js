9aa1153776dafa497035708e337bf71fee2565f1
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-22 09:53:33 +0200

    feat(AvailabilityModel): created initial model (#12)

---
+++ src/models/AvailabilityModel.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+const AvailabilityModel = sequelize.define('availability', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+
+    psychologist_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'psychologist',
+            key: 'user_id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    },
+
+    weekday: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        validate: {
+            min: 1,
+            max: 7,
+        },
+    },
+
+    start_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+    },
+
+    end_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+    },
+
+}, {
+    tableName: 'availability',
+    timestamps: false,
+    underscored: true,    
+});
+
+module.exports = AvailabilityModel;
