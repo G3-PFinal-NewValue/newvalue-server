8e906419f6c7d0263dec711a201d6ab3e0a50905
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-22 10:50:42 +0200

    feat(article): created model, controller and routes (#14)

---
+++ src/models/ArticleModel.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+const PsychologistModel = require('./PsychologistModel'); 
+
+const ArticleModel = sequelize.define('Article', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  author_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: PsychologistModel, 
+      key: 'user_id',
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  },
+
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+
+  published: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  published_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+}, {
+  tableName: 'articles',
+  timestamps: true, 
+  underscored: true,
+});
+
+module.exports = ArticleModel;
