5e566886cacc981d52f4249c3457ea387bef7d61
Camila Arenas <mcarenasdh@gmail.com>
2025-11-02 11:25:28 +0100

    feat(user): add GET endpoint to retrieve all users (admin)
    Adds the new  route to the  to allow administrators to fetch a list of all users.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { assignRole } from '../controllers/user.controller.js';
+import { assignRole, getAllUsers } from '../controllers/user.controller.js';
 import { createUserAndSendEmail } from '../controllers/firstSession.controller.js';
+//CA: Acá voy a importar los controladores y los middlewares que ya están definidos pero no importados. Arriba importé getAllUsers
+import authMiddleware from '../middleware/authMiddleware.js';
+import roleMiddleware from '../middleware/roleMiddleware.js';
 
 const userRouter = express.Router();
+
+// CA: Acá añado la ruta GET para que solo el admin pueda obtener todos los usuarios.
+userRouter.get('/', authMiddleware, roleMiddleware('admin'), getAllUsers);
 
 userRouter.patch('/assign-role', assignRole);
 
